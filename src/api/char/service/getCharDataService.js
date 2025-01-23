f07f4e62b19b27363587907545ffee82ed52696c
hangyu <jhg990508@gmail.com>
2025-01-23 17:32:53 +0900

    feat: 오래된 캐릭터 데이터 업데이트 로직 추가

--- src/api/char/service/getCharDataService.js
+++ src/api/char/service/getCharDataService.js
@@ -8,18 +8,27 @@ const getCharDataService = async (charNames) => {
     for (const charName of charNames) {
       try {
         let charData = await CharData.findOne({ character_name: charName });
+        const now = new Date();
 
         if (!charData) {
           const searchedCharData = await getCharData(charName);
           const newCharData = new CharData({
             ...searchedCharData,
-            lastUpdateDate: new Date().toISOString(),
+            lastUpdateDate: now.toISOString(),
           });
           await newCharData.save();
           charData = newCharData;
-        }
-
-        if (charData) {
+        } else {
+          const lastUpdateDate = new Date(charData.lastUpdateDate);
+          if (now - lastUpdateDate > 1000 * 60 * 60 * 24 * 7) {
+            console.log(`캐릭터 정보를 업데이트합니다. ${charName}`);
+            const updatedCharData = await getCharData(charName);
+            charData = Object.assign(charData, {
+              ...updatedCharData,
+              lastUpdateDate: now.toISOString(),
+            });
+            await charData.save();
+          }
           charDataList.push(charData);
         }
       } catch (error) {
